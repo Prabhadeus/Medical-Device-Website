a24bb1811c6a041d4a6efdfdf9fe9e11f3bfe67f
Prabhjot Singh <psingh37@pride.hofstra.edu>
2025-07-19 16:39:07 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="text-3xl font-bold text-blue-600 p-6">
+        Tailwind CSS is working!
+      </div>
+  );
+}
+
+export default App;
